60d7b1ec2bc58691b99d19aa9222088b00d0e822
zosmac <zosmac@github.com>
2025-07-29 07:29:22 -0400

    add help files

--- GraphvizSwift/AttributesDocView.swift
+++ GraphvizSwift/AttributesDocView.swift
@@ -9,59 +9,22 @@ import SwiftUI
 import WebKit
 
 @MainActor
-struct AttributesDocView: NSViewControllerRepresentable {
-    @Bindable var graph: Graph
+struct AttributesDocView: NSViewRepresentable {
+    var graph: Graph
     @Binding var kind: Int
     @Binding var row: Attribute.ID?
-
-    final class Coordinator: NSViewController { // }, WKUIDelegate {
-        init(_ docView: WKWebView) {
-            print("init web view")
-            super.init(nibName: nil, bundle: nil)
-            self.view = docView
-        }
-
-        override func loadView() {
-            super.loadView()
-        }
-
-        override func viewDidLoad() {
-            super.viewDidLoad()
-        }
-
-        @available(*, unavailable)
-        required init?(coder: NSCoder) {
-            print("coder init called for AttributesDocView.Controller")
-            fatalError("init(coder:) has not been implemented")
-        }
-        
-        func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
-            print("CONFIGURE WEBVIEW!", webView)
-            return webView
-        }
-        
-        func webViewDidClose(_ webView: WKWebView) {
-            print("CLOSED WEBVIEW!", webView)
-        }
-    }
-
+    
+    typealias Coordinator = Graph
     func makeCoordinator() -> Coordinator {
-        return Coordinator(graph.docView)
-    }
-
-    func makeNSViewController(context: Context) -> NSViewController {
-        let nsViewController = context.coordinator
-//        var nsView: NSView
-////        let view = WKWebView()
-////        view.uiDelegate = context.coordinator
-//        nsView = webView
-//
-//        nsViewController.view = nsView
-        return nsViewController
+        return graph
     }
     
-    func updateNSViewController(_ nsViewController: NSViewController, context: Context) {
-        let nsView = nsViewController.view as! WKWebView
+    func makeNSView(context: Context) -> NSView {
+        return context.coordinator.docView
+    }
+    
+    func updateNSView(_ nsView: NSView, context: Context) {
+        let nsView = nsView as! WKWebView
         let table = ParsedAttributes.shared.tables[kind]
         print("UPDATING DOC VIEW", nsView)
         if let row = row,
@@ -79,7 +42,81 @@ struct AttributesDocView: NSViewControllerRepresentable {
         }
     }
     
-    static func dismantleNSViewController(_ nsViewController: NSViewController, coordinator: Coordinator) {
-        print("dismantling AttributeDocView", nsViewController.view)
+    static func dismantleNSView(_ nsView: NSView, coordinator: Coordinator) {
+        print("dismantling AttributeDocView", nsView)
     }
+    
 }
+
+// seems like overkill, but retained in case
+//@MainActor
+//struct AttributesDocViewRepresentable: NSViewControllerRepresentable {
+//    var graph: Graph
+//    @Binding var kind: Int
+//    @Binding var row: Attribute.ID?
+//
+//    final class Coordinator: NSViewController { // , WKUIDelegate {
+//        var graph: Graph
+//
+//        init(graph: Graph) {
+//            self.graph = graph
+//            super.init(nibName: nil, bundle: nil)
+//        }
+//
+//        override func loadView() {
+//            super.loadView()
+//        }
+//
+//        override func viewDidLoad() {
+//            super.viewDidLoad()
+//        }
+//        
+//        @available(*, unavailable)
+//        required init?(coder: NSCoder) {
+//            print("coder init called for GraphAsPDF.Controller")
+//            fatalError("init(coder:) has not been implemented")
+//        }
+//        
+//        func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+//            print("CONFIGURE WEBVIEW!", webView)
+//            return webView
+//        }
+//        
+//        func webViewDidClose(_ webView: WKWebView) {
+//            print("CLOSED WEBVIEW!", webView)
+//        }
+//    }
+//
+//    func makeCoordinator() -> Coordinator {
+//        return Coordinator(graph: graph)
+//    }
+//
+//    func makeNSViewController(context: Context) -> NSViewController {
+//        let nsViewController = context.coordinator
+//        nsViewController.view = context.coordinator.graph.docView
+//        return nsViewController
+//    }
+//    
+//    func updateNSViewController(_ nsViewController: NSViewController, context: Context) {
+//        let nsView = nsViewController.view as! WKWebView
+//        let table = ParsedAttributes.shared.tables[kind]
+//        print("UPDATING DOC VIEW", nsView)
+//        if let row = row,
+//           let index = table.firstIndex(where: { $0.id == row }) {
+//            let name = table[index].name
+//            print(name)
+//            nsView.evaluateJavaScript("window.location.hash='#\(name)'") { (result: Any?, error: Error?) in
+//                if let result = result {
+//                    print("Result from JavaScript: \(result)")
+//                }
+//                if let error = error {
+//                    print("Error evaluating JavaScript: \(error)")
+//                }
+//            }
+//        }
+//    }
+//
+//    static func dismantleNSViewController(_ nsViewController: NSViewController, coordinator: Coordinator) {
+//        print("dismantling AttributeDocViewRepresentable", nsViewController.view)
+//    }
+//}
--- GraphvizSwift/GraphByType.swift
+++ GraphvizSwift/GraphByType.swift
@@ -51,67 +51,68 @@ struct GraphByType: NSViewRepresentable {
 }
 
 
-struct GraphRepresentable: NSViewControllerRepresentable {
-    var graph: Graph
-    var utType: UTType
-    
-    @Observable class Coordinator: NSViewController {
-        var graph: Graph
-
-        init(graph: Graph) {
-            self.graph = graph
-            super.init(nibName: nil, bundle: nil)
-        }
-
-        override func loadView() {
-            super.loadView()
-        }
-
-        override func viewDidLoad() {
-            super.viewDidLoad()
-        }
-        
-        @available(*, unavailable)
-        required init?(coder: NSCoder) {
-            print("coder init called for GraphAsPDF.Controller")
-            fatalError("init(coder:) has not been implemented")
-        }
-    }
-
-    func makeCoordinator() -> Coordinator {
-        return Coordinator(graph: graph)
-    }
-    
-    func makeNSViewController(context: Context) -> NSViewController {
-        let nsViewController = context.coordinator
-        var nsView: NSView
-        switch utType {
-        case UTType.svg:
-            let view = WKWebView()
-            nsView = view
-        case UTType.pdf:
-            let view = PDFView()
-            view.autoScales = true
-            nsView = view
-        default:
-            nsView = NSView()
-        }
-        graph.renderGraph(nsView: nsView)
-        
-        nsViewController.view = nsView
-        return nsViewController
-    }
-    
-    func updateNSViewController(_ nsViewController: NSViewController, context: Context) {
-        if graph.updated {
-            print("PDF View of graph updated!")
-            graph.renderGraph(nsView: nsViewController.view)
-        }
-    }
-    
-    static func dismantleNSViewController(_ nsViewController: NSViewController, coordinator: Coordinator) {
-        print("dismantle view controller called")
-        let graph = coordinator.graph
-        graph.closeGraph()
-    }
-}
+// seems like overkill, but retained in case
+//struct GraphRepresentable: NSViewControllerRepresentable {
+//    var graph: Graph
+//    var utType: UTType
+//    
+//    final class Coordinator: NSViewController {
+//        var graph: Graph
+//
+//        init(graph: Graph) {
+//            self.graph = graph
+//            super.init(nibName: nil, bundle: nil)
+//        }
+//
+//        override func loadView() {
+//            super.loadView()
+//        }
+//
+//        override func viewDidLoad() {
+//            super.viewDidLoad()
+//        }
+//        
+//        @available(*, unavailable)
+//        required init?(coder: NSCoder) {
+//            print("coder init called for GraphAsPDF.Controller")
+//            fatalError("init(coder:) has not been implemented")
+//        }
+//    }
+//
+//    func makeCoordinator() -> Coordinator {
+//        return Coordinator(graph: graph)
+//    }
+//    
+//    func makeNSViewController(context: Context) -> NSViewController {
+//        let nsViewController = context.coordinator
+//        var nsView: NSView
+//        switch utType {
+//        case UTType.svg:
+//            let view = WKWebView()
+//            nsView = view
+//        case UTType.pdf:
+//            let view = PDFView()
+//            view.autoScales = true
+//            nsView = view
+//        default:
+//            nsView = NSView()
+//        }
+//        graph.renderGraph(nsView: nsView)
+//        
+//        nsViewController.view = nsView
+//        return nsViewController
+//    }
+//    
+//    func updateNSViewController(_ nsViewController: NSViewController, context: Context) {
+//        if graph.updated {
+//            print("PDF View of graph updated!")
+//            graph.renderGraph(nsView: nsViewController.view)
+//        }
+//    }
+//    
+//    static func dismantleNSViewController(_ nsViewController: NSViewController, coordinator: Coordinator) {
+//        print("dismantle view controller called")
+//        let graph = coordinator.graph
+//        graph.closeGraph()
+//    }
+//}
