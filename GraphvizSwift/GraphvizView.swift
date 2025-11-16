e7d719d853cfefc47453b219023dfb00332d6736
zosmac <zosmac@gitlab.com>
2025-11-16 17:01:44 -0500

    Restore view scale to 1 when changing view type

--- GraphvizSwift/GraphvizView.swift
+++ GraphvizSwift/GraphvizView.swift
@@ -12,7 +12,7 @@ import SwiftUI
 struct GraphvizView: View {
     @Environment(\.openWindow) var openWindow
     @Environment(AttributesDocViewLaunch.self) var attributesDocViewLaunch
-    
+
     @Bindable var document: GraphvizDocument
     let url: URL?
     @Binding var position: ScrollPosition
@@ -30,69 +30,73 @@ struct GraphvizView: View {
         @Bindable var graph = Graph(text: document.text)
 
         ViewByType(document: document, url: url, viewType: viewType, rendering: graph.render(viewType: viewType, settings: settings), zoomScale: zoomScale, viewScale: $viewScale)
-        .inspector(isPresented: $inspectorPresented) {
-            AttributesView(graph: graph, settings: $settings, position: $position, attributesDocPage: attributesDocPage)
-                .inspectorColumnWidth(min: 200, ideal: 300, max: 400)
-        }
-        .focusedSceneValue(\.saveViewPresented, $saveViewPresented)
-        .focusedSceneValue(\.saveViewType, $viewType)
-        .sheet(isPresented: $saveViewPresented) {
-            SaveViewSheet(url: url, graph: graph, viewType: viewType, settings: settings)
-        }
-        .onAppear {
-            if attributesDocViewLaunch.firstTime {
-                openWindow(id: "AttributesDocView")
+            .onAppear {
+                if attributesDocViewLaunch.firstTime {
+                    openWindow(id: "AttributesDocView")
+                }
+                attributesDocViewLaunch.firstTime = false
             }
-            attributesDocViewLaunch.firstTime = false
-        }
-        .toolbarBackground(Color.accentColor.opacity(0.3))
-        .toolbar(id: "GraphvizViewToolbar") {
-            ToolbarItem(id: "View Type") {
-                ControlGroup("View Type") {
-                    Picker("View Type", selection: $viewType) {
-                        ForEach(viewableContentTypes, id: \.self) {
-                            Text($0.uppercased()).tag($0)
+            .onChange(of: viewType) {
+                zoomScale = 1.0
+                viewScale = 1.0
+            }
+            .inspector(isPresented: $inspectorPresented) {
+                AttributesView(graph: graph, settings: $settings, position: $position, attributesDocPage: attributesDocPage)
+                    .inspectorColumnWidth(min: 200, ideal: 300, max: 400)
+            }
+            .focusedSceneValue(\.saveViewPresented, $saveViewPresented)
+            .focusedSceneValue(\.saveViewType, $viewType)
+            .sheet(isPresented: $saveViewPresented) {
+                SaveViewSheet(url: url, graph: graph, viewType: viewType, settings: settings)
+            }
+            .toolbarBackground(Color.accentColor.opacity(0.3))
+            .toolbar(id: "GraphvizViewToolbar") {
+                ToolbarItem(id: "View Type") {
+                    ControlGroup("View Type") {
+                        Picker("View Type", selection: $viewType) {
+                            ForEach(viewableContentTypes, id: \.self) {
+                                Text($0.uppercased()).tag($0)
+                            }
+                        }
+                        .frame(width: 80)
+                        SaveViewButton(viewType: viewType)
+                    }
+                }
+                ToolbarItem(id: "Zoom") {
+                    ControlGroup("Zoom") {
+                        Button("Zoom out", systemImage: "minus.magnifyingglass") { // "arrow.down.right.and.arrow.up.left") {
+                            zoomScale /= 2.0.squareRoot()
+                        }
+                        Button("Actual Size", systemImage: "1.magnifyingglass") { // "arrow.uturn.left") {
+                            zoomScale = 1.0
+                        }
+                        Button("Zoom in", systemImage: "plus.magnifyingglass") { // "arrow.up.left.and.arrow.down.right") {
+                            zoomScale *= 2.0.squareRoot()
+                        }
+                        Button("Zoom to Fit", systemImage: "square.arrowtriangle.4.outward") { // "arrow.up.left.and.down.right.magnifyingglass") { // "arrow.up.left.and.down.right.and.arrow.up.right.and.down.left") {
+                            zoomScale = viewScale
                         }
                     }
-                    .frame(width: 80)
-                    SaveViewButton(viewType: viewType)
+                }
+                ToolbarItem(id: "Message") {
+                    Button("Message", systemImage: "exclamationmark.message") {
+                        messagePresented = graph.logMessage.message.isEmpty ? false : !messagePresented
+                    }
+                    .foregroundStyle(graph.logMessage.message.isEmpty ? .secondary : Color.red)
+                    .popover(isPresented: $messagePresented, arrowEdge: .trailing) {
+                        ScrollView {
+                            Text(graph.logMessage.message)
+                                .foregroundStyle(Color.accentColor)
+                        }
+                        .frame(minWidth: 200)
+                        .defaultScrollAnchor(.topLeading)
+                    }
+                }
+                ToolbarItem(id: "Attributes") {
+                    Button("Attributes", systemImage: "sidebar.trailing") {
+                        inspectorPresented.toggle()
+                    }
                 }
             }
-            ToolbarItem(id: "Zoom") {
-                ControlGroup("Zoom") {
-                    Button("Zoom out", systemImage: "minus.magnifyingglass") { // "arrow.down.right.and.arrow.up.left") {
-                        zoomScale /= 2.0.squareRoot()
-                    }
-                    Button("Actual Size", systemImage: "1.magnifyingglass") { // "arrow.uturn.left") {
-                        zoomScale = 1.0
-                    }
-                    Button("Zoom in", systemImage: "plus.magnifyingglass") { // "arrow.up.left.and.arrow.down.right") {
-                        zoomScale *= 2.0.squareRoot()
-                    }
-                    Button("Zoom to Fit", systemImage: "square.arrowtriangle.4.outward") { // "arrow.up.left.and.down.right.magnifyingglass") { // "arrow.up.left.and.down.right.and.arrow.up.right.and.down.left") {
-                        zoomScale = viewScale
-                    }
-                }
-            }
-            ToolbarItem(id: "Message") {
-                Button("Message", systemImage: "exclamationmark.message") {
-                    messagePresented = graph.logMessage.message.isEmpty ? false : !messagePresented
-                }
-                .foregroundStyle(graph.logMessage.message.isEmpty ? .secondary : Color.red)
-                .popover(isPresented: $messagePresented, arrowEdge: .trailing) {
-                    ScrollView {
-                        Text(graph.logMessage.message)
-                            .foregroundStyle(Color.accentColor)
-                    }
-                    .frame(minWidth: 200)
-                    .defaultScrollAnchor(.topLeading)
-                }
-            }
-            ToolbarItem(id: "Attributes") {
-                Button("Attributes", systemImage: "sidebar.trailing") {
-                    inspectorPresented.toggle()
-                }
-            }
-        }
     }
 }
