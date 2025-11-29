3c969d38510f539a8d48533f62bb85a5b99f4a2d
zosmac <zosmac@gitlab.com>
2025-11-29 12:14:31 -0500

    remove/comment out dead code of GraphvizDocument, LogInterceptor

--- GraphvizSwift/GraphvizDocument.swift
+++ GraphvizSwift/GraphvizDocument.swift
@@ -7,7 +7,6 @@
 
 import UniformTypeIdentifiers
 import SwiftUI
-//import Synchronization
 
 nonisolated
 extension UTType {
@@ -21,23 +20,13 @@ extension UTType {
 
 /// GraphvizDocument contains the contents of the file.
 struct GraphvizDocument: FileDocument {
-//@Observable final class GraphvizDocument: ReferenceFileDocument {
     static let readableContentTypes: [UTType] = [.gv, .canon]
     static let writableContentTypes: [UTType] = [.gv, .canon]
-//    struct Storage {
-//        var data: Data
-//    }
-//    let storage: Mutex<Storage>
 
     let docType: UTType
     var text: String
-//    var text: String {
-//        get { storage.withLock { String(data: $0.data, encoding: .utf8) ?? "" } }
-//        set { storage.withLock { $0.data = Data(newValue.utf8) } }
-//    }
 
     init() {
-//        self.storage = .init(.init(data: Data()))
         self.text = ""
         self.docType = .gv
     }
@@ -47,18 +36,8 @@ struct GraphvizDocument: FileDocument {
               let text = String(data: data, encoding: .utf8) else {
             throw CocoaError(.fileReadUnknown)
         }
-//        self.storage = .init(.init(data: data))
         self.text = text
         self.docType = configuration.contentType
-    }
-
-    func snapshot(contentType: UTType) throws -> Data {
-        Data(text.utf8)
-//        storage.withLock { $0.data }
-    }
-    
-    func fileWrapper(snapshot: Data, configuration: WriteConfiguration) throws -> FileWrapper {
-        return .init(regularFileWithContents: snapshot)
     }
 
     func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
--- GraphvizSwift/LogInterceptor.swift
+++ GraphvizSwift/LogInterceptor.swift
@@ -4,100 +4,100 @@
 //
 //  Created by Keefe Hayes on 6/16/25.
 //
-
-import os
-import SwiftUI
-
-/// LogObservee provides a place for log messages to be sent for display in the inspector view. This type is observable so that the posting of the message is detectcd by the Text view in InspectorView.
-@Observable nonisolated final class LogObservee: @unchecked Sendable {
-    var name: Notification.Name
-    var message: String = ""
-    init(name: Notification.Name) {
-        self.name = name
-    }
-}
-
-/// LogObserver awaits log message notifications from graphviz render operations.
-nonisolated final class LogObserver {
-    private let name: NSNotification.Name
-    let observer: Any
-    let observee: LogObservee
-
-    init(name: String) {
-        self.name = Notification.Name("GraphvizSwift.LogInterceptor.\(name)")
-        self.observee = LogObservee(name: self.name)
-        self.observer = NotificationCenter.default.addObserver(
-            forName: self.name,
-            object: nil,
-            queue: nil,
-            using: LogObserver.receiver(observee: observee))
-    }
-
-    deinit {
-        NotificationCenter.default.removeObserver(
-            observer,
-            name: name,
-            object: nil
-        )
-    }
-
-    static func receiver(observee: LogObservee) -> @Sendable (_ notification: Notification) -> Void {
-        return { @Sendable (notification) in
-            let string = notification.userInfo?["message"] as! String
-            observee.message = string
-        }
-    }
-    
-    func observe(name: String, block: () -> Any ) -> Any {
-        LogInterceptor.logLock.lock()
-        fflush(stderr)
-        dup2(LogInterceptor.shared.pipe.fileHandleForWriting.fileDescriptor, STDERR_FILENO)
-        LogInterceptor.shared.pipe.fileHandleForWriting.write(Data("\(name)\n".utf8))
-        let result = block()
-        LogInterceptor.shared.pipe.fileHandleForWriting.write(Data("\n".utf8))
-        dup2(LogInterceptor.shared.duperr, STDERR_FILENO)
-        LogInterceptor.logLock.unlock()
-        return result
-    }
-}
-
-/// logInterceptor prevents lazy loading of the global shared LogInterceptor.
-nonisolated
-let logInterceptor = LogInterceptor.shared
-
-/// LogInterceptor defines a pipe for capturing log messages sent to stderr.
-nonisolated
-struct LogInterceptor {
-    static let shared = LogInterceptor()
-    static let logLock = OSAllocatedUnfairLock()
-    let pipe = Pipe()
-    let duperr = dup(STDERR_FILENO)
-
-    private init() {
-//        let duperrHandle = FileHandle(fileDescriptor: duperr)
-        pipe.fileHandleForReading.readabilityHandler = { handle in
-            var data = Data()
-            while true {
-                data += handle.availableData
-                if data.count > 2 && // TODO: Scan for "\n\n" instead? Use additional exit string?
-                    data[data.endIndex-1] == 10 &&
-                    data[data.endIndex-2] == 10 {
-                    data = data.dropLast(2)
-                    let string = String(data: data, encoding: .utf8)!
-                    let splits = string.split(separator: "\n")
-                    let name = splits[0]
-                    let message = splits.count > 1 ? splits[1...].joined(separator: "\n") : ""
-//                    duperrHandle.write(Data("\(name): \(message)\n".utf8))
-                    if !message.isEmpty {
-                        NotificationCenter.default.post(
-                            name: Notification.Name("GraphvizSwift.LogInterceptor.\(name)"),
-                            object: nil,
-                            userInfo: ["message": message]
-                        )
-                    }
-                    data = Data()
-                }
-            }
-        }
-    }
-}
+//
+//import os
+//import SwiftUI
+//
+///// LogObservee provides a place for log messages to be sent for display in the inspector view. This type is observable so that the posting of the message is detectcd by the Text view in InspectorView.
+//@Observable nonisolated final class LogObservee: @unchecked Sendable {
+//    var name: Notification.Name
+//    var message: String = ""
+//    init(name: Notification.Name) {
+//        self.name = name
+//    }
+//}
+//
+///// LogObserver awaits log message notifications from graphviz render operations.
+//nonisolated final class LogObserver {
+//    private let name: NSNotification.Name
+//    let observer: Any
+//    let observee: LogObservee
+//
+//    init(name: String) {
+//        self.name = Notification.Name("GraphvizSwift.LogInterceptor.\(name)")
+//        self.observee = LogObservee(name: self.name)
+//        self.observer = NotificationCenter.default.addObserver(
+//            forName: self.name,
+//            object: nil,
+//            queue: nil,
+//            using: LogObserver.receiver(observee: observee))
+//    }
+//
+//    deinit {
+//        NotificationCenter.default.removeObserver(
+//            observer,
+//            name: name,
+//            object: nil
+//        )
+//    }
+//
+//    static func receiver(observee: LogObservee) -> @Sendable (_ notification: Notification) -> Void {
+//        return { @Sendable (notification) in
+//            let string = notification.userInfo?["message"] as! String
+//            observee.message = string
+//        }
+//    }
+//    
+//    func observe(name: String, block: () -> Any ) -> Any {
+//        LogInterceptor.logLock.lock()
+//        fflush(stderr)
+//        dup2(LogInterceptor.shared.pipe.fileHandleForWriting.fileDescriptor, STDERR_FILENO)
+//        LogInterceptor.shared.pipe.fileHandleForWriting.write(Data("\(name)\n".utf8))
+//        let result = block()
+//        LogInterceptor.shared.pipe.fileHandleForWriting.write(Data("\n".utf8))
+//        dup2(LogInterceptor.shared.duperr, STDERR_FILENO)
+//        LogInterceptor.logLock.unlock()
+//        return result
+//    }
+//}
+//
+///// logInterceptor prevents lazy loading of the global shared LogInterceptor.
+//nonisolated
+//let logInterceptor = LogInterceptor.shared
+//
+///// LogInterceptor defines a pipe for capturing log messages sent to stderr.
+//nonisolated
+//struct LogInterceptor {
+//    static let shared = LogInterceptor()
+//    static let logLock = OSAllocatedUnfairLock()
+//    let pipe = Pipe()
+//    let duperr = dup(STDERR_FILENO)
+//
+//    private init() {
+////        let duperrHandle = FileHandle(fileDescriptor: duperr)
+//        pipe.fileHandleForReading.readabilityHandler = { handle in
+//            var data = Data()
+//            while true {
+//                data += handle.availableData
+//                if data.count > 2 && // TODO: Scan for "\n\n" instead? Use additional exit string?
+//                    data[data.endIndex-1] == 10 &&
+//                    data[data.endIndex-2] == 10 {
+//                    data = data.dropLast(2)
+//                    let string = String(data: data, encoding: .utf8)!
+//                    let splits = string.split(separator: "\n")
+//                    let name = splits[0]
+//                    let message = splits.count > 1 ? splits[1...].joined(separator: "\n") : ""
+////                    duperrHandle.write(Data("\(name): \(message)\n".utf8))
+//                    if !message.isEmpty {
+//                        NotificationCenter.default.post(
+//                            name: Notification.Name("GraphvizSwift.LogInterceptor.\(name)"),
+//                            object: nil,
+//                            userInfo: ["message": message]
+//                        )
+//                    }
+//                    data = Data()
+//                }
+//            }
+//        }
+//    }
+//}
